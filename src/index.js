e3517fe58170df61b41e145a0bd2449aca0699d8
leMinhPhuong <phuongleminh841@gmail.com>
2024-04-29 21:21:14 +0700

    some of the basic structure

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { time } from "date-fns";
 import "./style.css";
 
 // create new project
@@ -6,29 +7,60 @@ class ProjectList {
   constructor() {
     this.listOfProject = [];
   }
-  get ProjectsList() {
+  get List() {
     return this.listOfProject;
-  }
-  set ProjectsList(tittle) {
-    const project = new Projects(tittle);
-    this.listOfProject.push(project);
   }
 }
 
-class Projects {
+const ProjectAdder = {
+  addProject(project) {
+    this.listOfProject.push(project);
+  },
+};
+
+const ProjectRemover = {
+  removeProject(removedProject) {
+    let index = this.listOfProject.findIndex(
+      (project) => project == removedProject
+    );
+    this.listOfProject.splice(index, 1);
+  },
+};
+
+Object.assign(ProjectList.prototype, ProjectAdder);
+Object.assign(ProjectList.prototype, ProjectRemover);
+
+class Project {
   title;
   description;
   todoList;
   constructor(title) {
     this.title = title;
+    this.todoList = [];
+  }
+  get todoList() {
+    return this.todoList;
   }
 }
-const notifyBanner = {
-  notify() {
-    console.log(`this is: ${this.title}`);
+
+const todoAdder = {
+  addTodoItem(toDoItem) {
+    this.todoList.push(toDoItem);
+    console.log("is this working");
   },
 };
-Object.assign(projects.prototype, notifyBanner);
+
+const todoRemover = {
+  removeTodoItem(removedToDoItem) {
+    let index = this.todoList.findIndex(
+      (toDoItem) => toDoItem == removedToDoItem
+    );
+    this.todoList.splice(index, 1);
+  },
+};
+
+Object.assign(Project.prototype, todoAdder);
+Object.assign(Project.prototype, todoRemover);
 
 class toDoItem {
   title;
@@ -41,5 +73,20 @@ class toDoItem {
     this.title = title;
   }
 }
-// const toDoItem1 = new toDoItem("this is my first todo-item");
-// console.log(toDoItem1.title);
+
+const tittleChanger = {
+  change(title) {
+    console.log(true);
+  },
+};
+
+const projectList = new ProjectList();
+
+console.log(projectList);
+
+const project1 = new Project("household");
+projectList.addProject(project1);
+
+const toDoItem1 = new toDoItem("laundry");
+project1.addTodoItem(toDoItem1);
+console.log(project1);
